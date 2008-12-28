03aa7a7534f3fba548565ca1150354b0a8b0c376
grosser <micha@micha.(none)>
2008-12-28 14:26:17 +0100

    refactoring + specs

--- init.rb
+++ init.rb
@@ -1,10 +1,1 @@
-class ActiveRecord::Base
-  def self.random(num=1,options={})
-    return [] if num.zero?
-    num_records = options.delete(:count) || count
-    highest_possible_offset = [0,num_records-num].max 
-    offset = [rand(num_records),highest_possible_offset].min
-    limit = [num,num_records].min
-    find(:all, {:offset => offset, :limit=>limit}.merge(options))
-  end
-end
+require 'random_records'
